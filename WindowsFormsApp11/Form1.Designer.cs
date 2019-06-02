d6d0849d39aa2f9d6a6673ca1a4db28a0de3d0f9
Kapustin Anton <kapustin.anton199712@gmail.com>
2019-06-02 18:00:13 +0700

    Летающие прямоугольники

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(677, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(82, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Не двигать";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(677, 27);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(106, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Передвигать 1й";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(677, 50);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(106, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Передвигать 2й";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(677, 73);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(106, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "Передвигать 3й";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        private Point _targetPosition;
+        private Point _targetPosition1;
+        private Point _targetPosition2;
+        private Point _Position;
+        private Point _Position1;
+        private Point _Position2;
+        private Point _direction = Point.Empty;
+        SolidBrush brush;
+        SolidBrush brush1;
+        SolidBrush brush2;
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Random rnd = new Random();
+            if (radioButton2.Checked == true)
+            {
+                _targetPosition.X += _direction.X;
+                _targetPosition.Y += _direction.Y;
+            }
+            if (radioButton3.Checked == true)
+            {
+                _targetPosition1.X += _direction.X;
+                _targetPosition1.Y += _direction.Y;
+            }
+            if (radioButton4.Checked == true)
+            {
+                _targetPosition2.X += _direction.X;
+                _targetPosition2.Y += _direction.Y;
+            }
+
+            var TargetRect = new Rectangle(_targetPosition.X, _targetPosition.Y, _Position.X, _Position.Y);
+            var TargetRect1 = new Rectangle(_targetPosition1.X, _targetPosition1.Y, _Position1.X, _Position1.Y);
+            var TargetRect2 = new Rectangle(_targetPosition2.X, _targetPosition2.Y, _Position2.X, _Position2.Y);
+
+            g.FillRectangle(brush, TargetRect);
+            g.FillRectangle(brush1, TargetRect1);
+            g.FillRectangle(brush2, TargetRect2);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(25, 500);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            _Position.X = r.Next(10, 250);
+            _Position1.X = r.Next(10, 250);
+            _Position2.X = r.Next(10, 250);
+            _Position.Y = r.Next(10, 250);
+            _Position1.Y = r.Next(10, 250);
+            _Position2.Y = r.Next(10, 250);
+            _targetPosition.X = r.Next(0, ClientSize.Width - _Position.X);
+            _targetPosition.Y = r.Next(0, ClientSize.Height - _Position.Y);
+            _targetPosition1.X = r.Next(0, ClientSize.Width - _Position1.X);
+            _targetPosition1.Y = r.Next(0, ClientSize.Height - _Position1.Y);
+            _targetPosition2.X = r.Next(0, ClientSize.Width - _Position2.X);
+            _targetPosition2.Y = r.Next(0, ClientSize.Height - _Position2.Y);
+            int numcolor = r.Next(3);
+            switch (numcolor)
+            {
+                case 0:
+                    brush = new SolidBrush(Color.Blue); break;
+                case 1:
+                    brush = new SolidBrush(Color.Yellow); break;
+                case 2:
+                    brush = new SolidBrush(Color.Red); break;
+            }
+            numcolor = r.Next(3);
+            switch (numcolor)
+            {
+                case 0:
+                    brush1 = new SolidBrush(Color.Blue); break;
+                case 1:
+                    brush1 = new SolidBrush(Color.Yellow); break;
+                case 2:
+                    brush1 = new SolidBrush(Color.Red); break;
+            }
+            numcolor = r.Next(3);
+            switch (numcolor)
+            {
+                case 0:
+                    brush2 = new SolidBrush(Color.Blue); break;
+                case 1:
+                    brush2 = new SolidBrush(Color.Yellow); break;
+                case 2:
+                    brush2 = new SolidBrush(Color.Red); break;
+            }
+
+
+        }
+    }
+}
